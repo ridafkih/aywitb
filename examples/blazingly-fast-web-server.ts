ea001094110eb79a67da17304f345d0242ecbd38
ridafkih <hello@rida.dev>
2026-03-26 16:27:49 -0400

    chore: add new example

---
+++ examples/blazingly-fast-web-server.ts
@@ -0,0 +1,10 @@
+import { aywitb } from "../index.ts";
+
+const { serve } = await aywitb<{ serve: (port: number) => Promise<void> }>(`
+  a blazingly fast web server that returns a random
+  defense that i can send in response to people who
+  are unsupportive of my amazing projects
+`, { verbose: true })
+
+await serve(3004);
+console.log("serving...");
